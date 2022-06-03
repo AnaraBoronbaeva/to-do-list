efc99b0c71b860a4a37c3d84849170ac1ef50b3b
Anara Boronbaeva <anara.boronbaeva.kk@gmail.com>
2022-06-04 02:57:48 +0600

    add file

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const result = document.getElementById("result");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (input.value === "") return;
+  createDeleteElements(input.value);
+  input.value = "";
+});
+
+function createDeleteElements(value) {
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  li.className = "li";
+  li.textContent = value;
+  result.appendChild(li);
+
+  li.addEventListener("click", (e) => {
+    e.preventDefault();
+    result.removeChild(li);
+  });
+}
